d58f57eac950bf590d4a0bdad303063f3620d0db
sajjad-007 <sajjadhossain8123@gmail.com>
2024-03-17 09:31:38 +0600

    react-toastify added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { getDatabase, push, ref, set,onValue,remove,update } from "firebase/database";
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const[text,setText] = useState("")
@@ -20,10 +22,30 @@ function App() {
     if (text !== "") {
       set(push(ref(db, 'add')), {
         myText: text,
-      });
+      }).then(()=>{
+        toast.success('Item created successfully', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
       setText("")
     } else {
-      console.log("enter your text");
+      toast.error('Please enter your text', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
     }
   }
   // read operation
@@ -41,23 +63,57 @@ function App() {
 
   // ==delete operation
   let handleDelete = (Deleteid) => {
-    remove(ref(db,'add/' + Deleteid));
+    remove(ref(db,'add/' + Deleteid)).then(()=>{
+      toast.success('Item deleted successfully',{
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    })
   }
   let handleEdit = (Editinfo) =>{
     setText(Editinfo.myText)
     setIsBtn(false)
     setTodoUpdate(Editinfo);
+    toast.error('Are you sure you want to update your text',{
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
     // todoUpdate(Deleteid)
   }
   let handleUpdate =(e)=>{
     e.preventDefault()
     update(ref(db, "add/" + todoUpdate.id),{
       myText: text,
+    }).then(()=>{
+      toast.success('Successfully updated your text', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     })
     setIsBtn(true)
     setText("")
   }
   return (
+    <>
+    <ToastContainer />
     <div className="bg-[#161A2B] w-[550px] m-auto mt-10 p-6 text-center text-white rounded-lg">
       <h1 className="text-2xl mt-[10px] mb-[26px] capitalize font-semibold">Write down your Daily activites</h1>
       <form className="relative w-[400px] mx-auto bg-[#161A2B]" >
@@ -84,6 +140,7 @@ function App() {
         </ul>
       </div>
     </div>
+    </>
   )
 }
 
